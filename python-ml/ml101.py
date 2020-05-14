f9b7f0a8799dfa27ad387a8f9301de2aea44da5c
DwanW <kunvang@yahoo.com>
2020-05-14 11:18:45 -0600

    implemented data machine learning with scikitlearning, image recognition with imageAI

--- python-ml/ml101.py
+++
@@ -1,6 +0,0 @@
-# import data
-# clean data (manage missing labels and field, so prediction is more accurate)
-# split data (divide into training set and test set, so human can determine if model is accurate)
-# create a model (import algorithms)
-# check output of the model with test set()
-# improve
